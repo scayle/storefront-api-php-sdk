5aadc4c8276ef5f0f2d274740dde025faa0c4a5d
Rubin Gjerovski <rubin.gerovski@gmail.com>
2023-04-21 09:32:28 +0200

    Update PHP SDK to 1.128.2

---
+++ lib/Models/CategoryProperty.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Scayle\StorefrontApi\Models;
+
+/**
+ * @property string $name 
+ * @property string|float $value 
+ */
+class CategoryProperty extends ApiObject
+{
+    protected $defaultValues = [
+        
+    ];
+
+    protected $classMap = [
+    ];
+
+    protected $collectionClassMap = [
+    ];
+
+    protected $collection2dClassMap = [
+    ];
+
+protected $polymorphic = [
+    ];
+
+    protected $polymorphicCollections = [
+    ];
+}
