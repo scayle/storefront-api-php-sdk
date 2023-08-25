85b6f321b6253617c462d557b588db6ad90f6dbe
sebastianhoitz <hoitz@komola.de>
2023-08-25 12:46:37 +0200

    Add missing class

---
+++ lib/Models/AppliedReductionAmount.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Scayle\StorefrontApi\Models;
+
+/**
+ * @property int $id
+ * @property string $key
+ * @property string $label
+ * @property string $type
+ * @property array $values
+ */
+class AppliedReductionAmount extends ApiObject
+{
+    protected $defaultValues = [
+
+    ];
+
+    protected $classMap = [
+    ];
+
+    protected $collectionClassMap = [
+    ];
+
+    protected $collection2dClassMap = [
+    ];
+
+    protected $polymorphic = [
+    ];
+
+    protected $polymorphicCollections = [
+    ];
+}
